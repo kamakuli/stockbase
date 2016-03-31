4305a6cdf25412ff59f3f5a88712d352ce1a04b7
xuezhen <xuezhen@skyworth.com>
2016-03-31 12:24:19 +0800

    complete test

--- Main_SearchGoodStock.m
+++ Main_SearchGoodStock.m
@@ -1,6 +1,6 @@
 %% Main_SearchGoodStock.m
 %% set stock code
-    StockC = '002751'
+    StockC = '002751';
     StockIndex = str2num(StockC);
 %% load list
         %load StockList.mat
@@ -14,25 +14,19 @@
             disp('directory does not exist')
             return;
         end
-
-        prefix = '';
-        
+        prefix = '';        
         if( StockIndex >= 600000 ) && ( StockIndex < 605000)
             prefix = 'sh';
-        end
-        
+        end        
         if( StockIndex > 000000 ) && ( StockIndex < 003000)
             prefix = 'sz';
-        end
-        
+        end        
         if( StockIndex > 300000 ) && ( StockIndex < 301000)
             prefix = 'sz';
-        end          
-
+        end
 
         %Scode = StockCode{i};
-        %Sname = StockName{i};
-       
+        %Sname = StockName{i};       
         %FileStringD_Ex = [FolderStrD_Ex,'/',StockCode{i},'_D_ExDiv.mat'];
         FileString = [FolderStr,'/',prefix,StockC,'_D_ForwardAdj.mat'];
 %% load mat file
@@ -43,5 +37,28 @@
         str=['start data: ',num2str(StockData(1,1))];
         disp(str);
         str=['end data  : ',num2str(StockData(end,1))];
-        disp(str);     
-        s = StockData(:,5);
+        disp(str);
+%%        
+        %calculate 5 average
+        n = 5;
+        Len = length(StockData(:,5));
+        s = CalculateStockAverage(StockData(:,5), n);
+        %plot(s);
+%%     ÕÒÉÏÉýÖÜÆÚ
+        start = n+1;
+        l1 = 0;
+        sum = 0;
+        for i=n+1:Len,
+            if( s(i) > s(i-1) )
+                l1 = l1+1;
+            else
+                if( (l1 > 25) )
+                    disp(['find a up timezone, start at:',num2str(StockData(i-l1,1)),'      end at:',num2str(StockData(i,1)), '   last ',num2str(l1),' days']);                    
+                    start = i;
+                    l1 = 0;
+                    sum = sum + 1;
+                end
+            end
+        end
+        disp(['There are ', num2str(sum), ' up cycles']);
+        
